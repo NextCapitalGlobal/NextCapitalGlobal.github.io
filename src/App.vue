74c9ddcd1746e5b1e0c59392f74273f2a36581d5
Left <left@left.tw>
2023-03-16 23:00:08 +0800

    add desktop ver

---
+++ src/App.vue
@@ -0,0 +1,152 @@
+<template>
+  <!-- nav -->
+  <div class="header"><div class="inner clearfix">
+    <a href="" class="float-left"><img src="./assets/logo.svg"></a>
+    <div class="float-right">
+      <div class="lang-block inline-block mr-6">
+        <img class="inline-block" src="./assets/icon-lang.svg">
+        <select>
+          <option>中文</option>
+          <option>English</option>
+        </select>
+      </div>
+      <a href="" class="btn-black">聯繫我們</a>
+    </div>
+    <nav>
+      <ul class="text-center">
+        <li class="inline-block mr-8"><a href="">關於我們</a></li>
+        <li class="inline-block mr-8"><a href="">最新消息</a></li>
+        <li class="inline-block"><a href="">聯繫我們</a></li>
+      </ul>
+    </nav>
+  </div></div>
+  <!-- main -->
+  <div class="main-container">
+    <p class="mb-2">Next Capital</p>
+    <h1 class="mb-6">Next Capital <br>推動<p class="highlight">金融科技</p>未來發展</h1>
+    <div class="w-3/5">
+      <p>Next Capital 是一間創業投資公司（Venture Capital, VC），投資金融科技（Financial Technology, FinTech）相關有潜力的公司，推動金融科技未來發展，包含支付、P2P 借貸、區塊鏈、財富管理、保險科技等等的金融服務。</p>
+    </div>
+  </div>
+  <div class="card-container clearfix">
+    <img class="float-right" src="./assets/card-yellow.png">
+    <img class="float-right" src="./assets/card-blue.png">
+  </div>
+  <div class="feature-container">
+    <p class="mb-10">安全可靠的 NEXT<br>協助你</p>
+    <div class="flex flex-row">
+      <div class="basis-1/3 feature-block flex flex-nowrap">
+        <button class="flex-none icon"><img src="./assets/icon-money.svg"></button>
+        <div>
+          <p class="title">擁有你的 MasterCard</p>
+          <p>Next Capital 發行 MasterCard （萬事達卡）信用卡。</p>
+        </div>
+      </div>
+      <div class="basis-1/3 feature-block flex flex-nowrap">
+        <button class="flex-none icon"><img src="./assets/icon-pay.svg"></button>
+        <div>
+          <p class="title">代收代付服務 X Pay</p>
+          <p>X Pay 提供企業合作方案,提供快速收款連結，支援 crypto 代收代付，讓商家也能向消費者收取 crypto。</p>
+        </div>
+      </div>
+      <div class="basis-1/3 feature-block flex flex-nowrap">
+        <button class="flex-none icon"><img src="./assets/icon-grow.svg"></button>
+        <div>
+          <p class="title">數位支票 BitCheck</p>
+          <p>為您的跨境貿易與國際支付，提供最安全的免費線上履約保證。</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- news -->
+  <div class="news-container">
+    <div class="clearfix mb-8">
+      <h3 class="float-left">最新消息</h3>
+      <div class="float-right">
+        <button class="btn-prev"><img src="./assets/icon-prev.svg"></button>
+        <button class="btn-next"><img src="./assets/icon-next.svg"></button>
+      </div>
+    </div>
+    <div class="flex">
+      <div class="news-block"><a href="" target="_blank">
+        <div class="thumb-block"><img src="./assets/news-thumb.png"></div>
+        <div class="content-block">
+          <p class="title">新戶刷卡或完成任務享最高回饋NT$486</p>
+          <p>活動期間<br>2022-01-22-2022-12-30</p>
+          <p>帳戶啟用後次月底前符合以下任一情境，即享現金回饋NT$366...</p>
+        </div>
+      </a></div>
+      <div class="news-block"><a href="" target="_blank">
+        <div class="thumb-block"><img src="./assets/news-thumb.png"></div>
+        <div class="content-block">
+          <p class="title">新戶刷卡或完成任務享最高回饋NT$486</p>
+          <p>活動期間<br>2022-01-22-2022-12-30</p>
+          <p>帳戶啟用後次月底前符合以下任一情境，即享現金回饋NT$366...</p>
+        </div>
+      </a></div>
+      <div class="news-block"><a href="" target="_blank">
+        <div class="thumb-block"><img src="./assets/news-thumb.png"></div>
+        <div class="content-block">
+          <p class="title">新戶刷卡或完成任務享最高回饋NT$486</p>
+          <p>活動期間<br>2022-01-22-2022-12-30</p>
+          <p>帳戶啟用後次月底前符合以下任一情境，即享現金回饋NT$366...</p>
+        </div>
+      </a></div>
+      <!-- <div class="news-block"><a href="" target="_blank">
+        <div class="thumb-block"><img src="./assets/news-thumb.png"></div>
+        <div class="content-block">
+          <p class="title">新戶刷卡或完成任務享最高回饋NT$486</p>
+          <p>活動期間<br>2022-01-22-2022-12-30</p>
+          <p>帳戶啟用後次月底前符合以下任一情境，即享現金回饋NT$366...</p>
+        </div>
+      </a></div> -->
+    </div>
+  </div>
+  <!-- intro -->
+  <div class="intro-container">
+    <h3 class="text-center">擁有你的 <p class="highlight">MasterCard</p></h3>
+    <div class="intro-block">
+      <div class="cover-block"><img src="./assets/card-screenshot.svg"></div>
+      <div class="content-block">
+        <p>新辦用戶享有低手續費率。</p>
+        <p>在 Next Capital 集團下使用所有服務，享有額外優惠。</p>
+        <a href="" class="btn-black mt-6">即將上市</a>
+      </div>
+    </div>
+  </div>
+  <!-- contact -->
+  <div class="contact-container">
+    <div class="inquiry-block clearfix">
+      <h3>對 Next Capital 有興趣嗎？</h3>
+      <a href="" class="float-right btn-white">聯絡我們</a>
+      <p class="mt-5">立即諮詢業務專員</p>
+    </div>
+    <div class="subscribe-block">
+      <h3 class="m-0 mb-8">關注我們的最新動態</h3>
+      <label class="subscribe-form">
+        <img src="./assets/icon-mail-border.svg" class="pointer-events-none icon">
+        <input type="email" placeholder="輸入您的信箱" class="w-1/2">
+        <button class="btn-white">訂閱</button>
+      </label>
+    </div>
+  </div>
+  <!-- footer -->
+  <footer><div class="clearfix">
+    <img src="./assets/logo-white.svg" class="float-left logo">
+    <ul class="float-right">
+      <li class="inline-block mr-2.5"><a href="" target="_blank"><img src="./assets/icon-mail.svg"></a></li>
+      <li class="inline-block"><a href="" target="_blank"><img src="./assets/icon-facebook.svg"></a></li>
+    </ul>
+    <nav>
+      <ul class="text-center">
+        <li class="inline-block mr-8"><a href="">關於我們</a></li>
+        <li class="inline-block mr-8"><a href="">最新消息</a></li>
+        <li class="inline-block"><a href="">聯繫我們</a></li>
+      </ul>
+    </nav>
+  </div></footer>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>

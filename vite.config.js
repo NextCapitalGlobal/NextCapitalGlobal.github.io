81548e96521fa202a611d57458f63015523ba313
Left <left@left.tw>
2023-04-04 20:40:36 +0800

    fix bug: output filename bug

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 import { resolve } from 'path';
 
-// https://vitejs.dev/config/
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -17,11 +20,14 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         en: resolve(__dirname, 'en.html')
       },
-      // output: {
-      //   entryFileNames: `assets/[name].js`,
-      //   chunkFileNames: `assets/[name].js`,
-      //   assetFileNames: `assets/[name].[ext]`
-      // }
+      output: {
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : '';
+          
+          return driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '');
+        }
+      }
     }
   }
 })
